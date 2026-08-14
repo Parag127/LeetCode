8760b3978940c91bb833d56720319086d90a8f9e
Parag Badgujar <badgujarparag419@gmail.com>
2026-08-14 09:42:38 +0530

    Time: 0 ms (100%), Space: 54.2 MB (28.1%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let i = j = 0;
+    let k = nums.length - 1;
+
+    while (i <= k) {
+        if (nums[i] == 0)  {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            i++;
+            j++;
+        } else if (nums[i] == 2) {
+            [nums[i], nums[k]] = [nums[k], nums[i]];
+            k--;
+        } else {
+            i++;
+        }
+    }
+};
