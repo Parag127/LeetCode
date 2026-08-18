3068bb04a75901c0bf095e7a273e593a95f0d586
Parag Badgujar <badgujarparag419@gmail.com>
2026-08-18 21:16:14 +0530

    Time: 0 ms (100%), Space: 45.6 MB (15.96%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -11,16 +11,16 @@
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null || head.next == null) return head;
-        ListNode temp = head;
         
+        ListNode temp = head;
         while (temp.next != null) {
             if (temp.val == temp.next.val) {
                 temp.next = temp.next.next;
             } else {
+
             temp = temp.next;
             }
         }
-
         return head;
     }
 }
