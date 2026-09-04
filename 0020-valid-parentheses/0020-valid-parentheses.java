cfe993d7fde51ebcb716289d3bb95498ddbf281c
Parag Badgujar <badgujarparag419@gmail.com>
2026-09-05 01:56:28 +0530

    Time: 4 ms (36.54%), Space: 43.7 MB (9.85%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -5,17 +5,19 @@ class Solution {
         Stack<Character> st = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
                 st.push(s.charAt(i));
             } else {
                 if (st.isEmpty()) return false;
-
                 char ch = s.charAt(i);
-                if (st.peek() == '[' && ch == ']' || st.peek() == '{' && ch == '}' || st.peek() == '(' && ch == ')') st.pop();
-                else return false;
+
+                if ((st.peek() == '(' && ch == ')') || (st.peek() == '[' && ch == ']') || (st.peek() == '{' && ch == '}')) {
+                    st.pop();
+                } else return false;
             }
         }
-        if (st.isEmpty()) return true;
+
+        if(st.isEmpty()) return true;
         else return false;
     }
 }
