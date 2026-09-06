076e888466e4f15ce3d3216711e953fe2613de6a
Parag Badgujar <badgujarparag419@gmail.com>
2026-09-07 01:18:25 +0530

    Time: 0 ms (100%), Space: 47.4 MB (90.31%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,38 +1,32 @@
 class Solution {
-    public int trap(int[] height) {
-        int n = height.length;
-
+    public int trap(int[] arr) {
+        int n = arr.length;
+        
         if (n <= 2) return 0;
-
-        int[] prefix = new int[n];
-        int[] suffix = new int[n];
-
-        prefixMax(prefix, height, n);
-        suffixMax(suffix, height, n);
-
+        
+        int leftMax = 0;
+        int rightMax = 0;
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            int leftMax = prefix[i];
-            int rightMax = suffix[i];
-
-            ans += Math.min(leftMax, rightMax) - height[i];
-        }        
+        int l = 0;
+        int r = n - 1;
+        
+        while (l < r) {
+            if (arr[l] < arr[r]) {
+                if (leftMax > arr[l]) {
+                    ans += leftMax - arr[l];
+                } else {
+                    leftMax = arr[l];
+                }
+                l++;
+            } else {
+                if (rightMax > arr[r]) {
+                    ans += rightMax - arr[r];
+                } else {
+                    rightMax = arr[r];
+                }
+                r--;
+            }
+        }
         return ans;
-    }
-
-    public static void prefixMax(int[] prefix, int[] arr, int n){
-        prefix[0] = arr[0];
-
-        for (int i = 1; i < n; i++) {
-            prefix[i] = Math.max(prefix[i - 1], arr[i]);
-        }
-    }
-
-    public static void suffixMax(int[] suffix, int[] arr, int n){
-        suffix[n - 1] = arr[n - 1];
-
-        for (int i = n - 2; i >= 0; i--) {
-            suffix[i] = Math.max(suffix[i + 1], arr[i]);
-        }
     }
 }
