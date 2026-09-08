8acca3e00edbb05c643fdec692fe6f12ca6f42ee
Parag Badgujar <badgujarparag419@gmail.com>
2026-09-08 11:12:56 +0530

    Time: 5 ms (65.87%), Space: 46.7 MB (81.31%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        
+        int n = asteroids.length;
+        Stack<Integer> st= new Stack<>();
+        int i = 0;
+        while (i < n) {
+
+            int aste = asteroids[i];
+            boolean destroy = false;
+
+            while (!st.isEmpty() && aste < 0 && st.peek() > 0) {
+                if (st.peek() < Math.abs(aste)) {
+                    st.pop();
+                } else if (st.peek() == Math.abs(aste)){ 
+                    st.pop();
+                    destroy = true;
+                    break;
+                } else {
+                    destroy = true;
+                    break;
+                }
+            }
+
+            if (!destroy) {
+                st.push(aste);
+            }
+            i++;
+        }
+        int[] destroyedAste = new int[st.size()];
+
+        for (int x = destroyedAste.length - 1; x >= 0; x--) {
+            destroyedAste[x] = st.peek();
+            st.pop();
+        }
+        return destroyedAste;
+    }
+}
