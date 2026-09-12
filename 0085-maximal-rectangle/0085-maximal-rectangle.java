9f3914fe5ad7e0a0fa707aa4ae436ca12bf6cec0
Parag Badgujar <badgujarparag419@gmail.com>
2026-09-12 23:02:59 +0530

    Time: 6 ms (94.64%), Space: 51.3 MB (23.88%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,50 +1,44 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        int[] height = new int[matrix[0].length];
-
+        int[] heights = new int[matrix[0].length];
         int maxArea = 0;
-
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] == '1') {
-                    height[j] += 1;
+                    heights[j] += 1;
                 } else {
-                    height[j] = 0;
+                    heights[j] = 0;
                 }
             }
-            maxArea = Math.max(maxArea, largestRectangle(height));
-        }
 
+            maxArea = Math.max(maxArea, findLargestRectangle(heights));
+        }
         return maxArea;
     }
 
-    public static int largestRectangle(int[] arr) {
+    static int findLargestRectangle(int[] arr) {
         int n = arr.length;
         Deque<Integer> st = new ArrayDeque<>();
-
         int maxArea = 0;
+        int i = 0;
 
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
-                int el = st.peek();
-                st.pop();
-
+        while(i < n) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                int el = st.pop();
                 int NSE = i;
                 int PSE = st.isEmpty() ? -1 : st.peek();
 
-                maxArea = Math.max(maxArea, arr[el] * (NSE - PSE - 1));
+                maxArea = Math.max(maxArea, (NSE - PSE - 1) * arr[el]);
             }
             st.push(i);
+            i++;
         }
 
         while (!st.isEmpty()) {
-            int el = st.peek();
-            st.pop();
-
-            int NSE = n;
+            int el = st.pop();
+            int NSE = i;
             int PSE = st.isEmpty() ? -1 : st.peek();
-            
-            maxArea = Math.max(maxArea, arr[el] * (NSE - PSE - 1));
+            maxArea = Math.max(maxArea, (NSE - PSE - 1) * arr[el]);
         }
         return maxArea;
     }
