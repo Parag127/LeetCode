a0b3ce202fb48cecb48072f2a577e0e8438ccfa1
Parag Badgujar <badgujarparag419@gmail.com>
2026-09-12 17:47:29 +0530

    Time: 82 ms (7.6%), Space: 48.2 MB (15.83%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -1,43 +1,38 @@
 class Solution {
     public String removeKdigits(String num, int k) {
-        if (k == num.length()) return "0";
-
         Deque<Character> st = new ArrayDeque<>();
+        int n = num.length();
         int i = 0;
-
-        while (i < num.length()) {
-            while (!st.isEmpty() && k > 0 && st.peek() > num.charAt(i)) {
+        while (i < n) {
+            while (!st.isEmpty() && k > 0 && st.peek() > num.charAt(i)){
                 st.pop();
                 k--;
             }
+
             st.push(num.charAt(i));
             i++;
         }
-
-        while (k > 0 && !st.isEmpty()) {
+        while (!st.isEmpty() && k > 0) {
             st.pop();
             k--;
         }
 
         if (st.isEmpty()) return "0";
 
-        StringBuilder ans = new StringBuilder();
-
-        while (!st.isEmpty()) {
-            ans.append(st.removeLast());
+        String ans = new String();
+        i = st.size();
+        for (int x = 0; x < i; x++) {
+            ans += st.removeLast();
         }
 
         i = 0;
-
-        while (i < ans.length() && ans.charAt(i) == '0') {
+        int count = 0;
+        while (i < ans.length() && ans.charAt(i) == '0'){
+            count++;
             i++;
         }
+        if (i == ans.length()) return "0";
 
-        if (i == ans.length()) {
-            return "0";
-        }
-
-        return ans.substring(i);
-        
+        return ans.substring(count);
     }
 }
