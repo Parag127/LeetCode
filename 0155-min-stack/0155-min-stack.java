1869ae4b230e299f667a56e99fe269356bf50b73
Parag Badgujar <badgujarparag419@gmail.com>
2026-09-14 15:36:47 +0530

    Time: 27 ms (98.9%), Space: 102.5 MB (18.38%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,53 +1,49 @@
 class MinStack {
-
-    long min = Integer.MAX_VALUE;
     Stack<Long> st = new Stack<>();
-
+    long min = Integer.MAX_VALUE;
     public MinStack() {
+        st = new Stack<>();
     }
-
+    
     public void push(int value) {
-        long val = value;
+        long val = value * 1L;
         if (st.isEmpty()) {
             st.push(val);
             min = val;
         } else {
-            if (val > min)
+            if (val > min) {
                 st.push(val);
-            else {
-                min = 2 * val - min;
-                st.push(min);
+            } else {
+                st.push(2 * val - min);
                 min = val;
             }
         }
-
     }
-
+    
     public void pop() {
-        if (st.isEmpty())
+        if (st.isEmpty()) {
             return;
-
-        long x = st.peek();
-        st.pop();
-
-        if (x < min)
-            min = 2 * min - x;
-    }
-
-    public int top() {
-        if (st.isEmpty())
-            return -1;
-
-        long x = st.peek();
-        if (x > min) {
-            return (int)x;
-        } else {
-            return (int)min;
+        }
+        Long x = st.pop();
+        if (min > x) {
+            min = 2 * min - x;   
         }
     }
+    
+    public int top() {
+        if(st.isEmpty()) return -1;
 
+        long x = st.peek();
+
+        if (x > min) {
+            return (int)(x);
+        } else {
+            return (int)(min);
+        }
+    }
+    
     public int getMin() {
-        return (int)min;
+        return (int)(min);
     }
 }
 
