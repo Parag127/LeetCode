6974a61202340634747a20e633bde2590dc882d1
Parag Badgujar <badgujarparag419@gmail.com>
2026-09-14 15:45:03 +0530

    Time: 3 ms (58.69%), Space: 52.7 MB (26.64%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -6,11 +6,11 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 1) {
                 count += 1;
+                max = Math.max(max, count);
             } else {
                 count = 0;
             }
 
-            max = Math.max(max, count);
         }
         return max;
     }
