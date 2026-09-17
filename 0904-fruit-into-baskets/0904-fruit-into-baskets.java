bd5215279c862a0832d1a4a5e7fae04fe3a9e8e2
Parag Badgujar <badgujarparag419@gmail.com>
2026-09-17 22:51:20 +0530

    Time: 63 ms (5.11%), Space: 73.2 MB (6.81%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int l = 0, r = 0, maxLen = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        while (r < fruits.length) {
+            if (map.containsKey(fruits[r])) {
+                map.put(fruits[r], map.get(fruits[r]) + 1);
+            } else {
+                map.put(fruits[r], 1);
+            }
+
+            if (map.size() > 2) {
+                while (map.size() > 2) {
+                    map.put(fruits[l], map.get(fruits[l]) - 1);
+
+                    if (map.get(fruits[l]) == 0) {
+                        map.remove(fruits[l]);
+                    }
+                    l++;
+                }
+            }
+
+            if (map.size() <= 2) {
+                maxLen = Math.max(maxLen, r - l + 1);
+            }
+            r++;
+        }
+        return maxLen;
+    }
+}
