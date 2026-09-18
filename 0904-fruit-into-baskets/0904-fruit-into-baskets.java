420589688c537ad52ae798ab1cb97debea681805
Parag Badgujar <badgujarparag419@gmail.com>
2026-09-18 16:18:41 +0530

    Time: 61 ms (7.7%), Space: 70.1 MB (75.54%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -14,11 +14,10 @@ class Solution {
             }
 
             if (map.size() > 2) {
-                while (map.size() > 2) {
-                    map.put(fruits[l], map.get(fruits[l]) - 1);
-                    if (map.get(fruits[l]) == 0) map.remove(fruits[l]);
-                    l++;
-                }
+                map.put(fruits[l], map.get(fruits[l]) - 1);
+                if (map.get(fruits[l]) == 0)
+                    map.remove(fruits[l]);
+                l++;
             } else {
                 maxLen = Math.max(maxLen, r - l + 1);
             }
