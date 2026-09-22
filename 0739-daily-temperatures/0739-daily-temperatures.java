842fa5afc9280c0bc3a845ef93bda28a152ccf0c
Parag Badgujar <badgujarparag419@gmail.com>
2026-09-23 01:01:08 +0530

    Time: 24 ms (85.26%), Space: 106.8 MB (56.74%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,22 +1,21 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        int n = temperatures.length;
-        int[] NGT = new int[n];
-        return findNGT(temperatures, NGT); 
+        int[] NWT = new int[temperatures.length];
+        return findNWT(temperatures, NWT);
     }
 
-
-    static int[] findNGT(int[] arr, int[] NGE) {
+    static int[] findNWT(int[] arr, int[] NGE) {
         int n = arr.length;
-        Deque<Integer> st = new ArrayDeque<>();
         int i = n - 1;
 
+        Deque<Integer> st = new ArrayDeque<>();
         while (i >= 0) {
-            while (!st.isEmpty() && arr[st.peek()]  <= arr[i]){
+            while (!st.isEmpty() && arr[st.peek()] <= arr[i]) {
                 st.pop();
             }
 
             NGE[i] = st.isEmpty() ? 0 : st.peek() - i;
+
             st.push(i);
             i--;
         }
